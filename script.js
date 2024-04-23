81b80aaf25ee42a7919bd226dce69822e03a1b4e
Sajid <sajidmohammad460519@gmail.com>
2024-04-24 07:16:40 +0800

    able to do simple calculations, features to add: add digit 0... add ac buttonn where people can clear screen, add feature where user can add multiple numbers

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-const buttons = document.querySelectorAll('button')
-const display = document.querySelector('.display')
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
 
-let firstDigit = ""
-let secondDigit = ""
-let operand = false
-let outputValue = 0
+let firstDigit = "";
+let secondDigit = "";
+let operand = false;
+let outputValue = 0;
 let operator;
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        display.textContent = display.textContent + button.id
-        if (!Number.isNaN(button.id) && operand == false) {
-            firstDigit = firstDigit + button.id
-        }
-        else if (operand == true && !Number.isNaN(button.id)) {
-            secondDigit = secondDigit + button.id
-        }
-        else if (button.id == '=') {
-            if (operator == '+') {
-                alert(firstDigit + secondDigit)
-            }
-            else if (operator == '-') {
-                alert(firstDigit - secondDigit)
-            }
-            else if (operator == 'x') {
-                alert(firstDigit * secondDigit)
-            }
-            else if (operator == '/') {
-                alert(firstDigit / secondDigit)
-            }
-        }
-        else if (button.id == '+' || button.id == '-' || button.id == 'x' || button.id == '/') {
-            operator = button.id;
-            operand = true;
-        }
-    })
-})
+  button.addEventListener("click", () => {
+    if (firstDigit == "") {
+      display.textContent = "";
+    }
+    display.textContent = display.textContent + button.id;
+    if (isNumeric(button.id) && operand == false) {
+      firstDigit = firstDigit + button.id;
+    } else if (operand == true && isNumeric(button.id)) {
+      secondDigit = secondDigit + button.id;
+    } else if (button.id == "=") {
+      display.textContent = "";
+      if (operator == "+") {
+        display.textContent = parseInt(firstDigit) + parseInt(secondDigit);
+      } else if (operator == "-") {
+        display.textContent = parseInt(firstDigit) - parseInt(secondDigit);
+      } else if (operator == "x") {
+        display.textContent = parseInt(firstDigit) * parseInt(secondDigit);
+      } else if (operator == "/") {
+        display.textContent = parseInt(firstDigit) / parseInt(secondDigit);
+      }
+      firstDigit = "";
+      secondDigit = "";
+      operand = false;
+    } else if (
+      button.id == "+" ||
+      button.id == "-" ||
+      button.id == "x" ||
+      button.id == "/"
+    ) {
+      operator = button.id;
+      operand = true;
+    }
+  });
+});
 
+function isNumeric(str) {
+  return !isNaN(parseFloat(str)) && isFinite(str);
+}
